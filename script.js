e152146fafa6bbc55f47dbe65d9a124bb2d2d9ec
Sam Patterson <swpatterson.wm@gmail.com>
2018-02-09 13:45:59 -0500

    update to fixed grid size with create grid button

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 const container = document.querySelector('.container')
-const grid = document.createElement('div');
-grid.classList.add('grid');
+//const grid = document.createElement('div');
+//grid.classList.add('grid');
 
 function createGrid(x) {
   var i = 0
   while (i<(x*x)) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
+    grid.style.height = 500/x-2 +'px'
+    grid.style.width = 500/x-2 + 'px'
     grid.addEventListener('mouseover', () => {
       grid.style.backgroundColor = "black"})
     container.appendChild(grid);
     i++
   }
-  container.style.height = 22*x+'px'
-  container.style.width = 22*x+'px'
 }
-createGrid(30)
+
+function gridSize() {
+  var i = container.childElementCount
+    for(; i>0; i--) {
+      clearGrid();
+    }
+  var size = prompt("What size grid do you want?")
+  if (size == null || size == "") {
+    return
+  } else {
+    x = size
+    createGrid(x)
+  }
+}
+
+
+function clearGrid() {
+  const grid = document.querySelector('.grid')
+  grid.parentNode.removeChild(grid);
+  }
